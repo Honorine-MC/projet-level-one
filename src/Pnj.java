d10be56d924fdcb18cf0fc449e58efc908a2908d
honorine <honorine.mariechristy@gmail.com>
2021-05-17 10:28:25 +0200

    Implémentation de la classe Pnj

--- src/Pnj.java
+++ src/Pnj.java
@@ -1,4 +1,36 @@
 
-public class Pnj {
+public class Pnj extends Personnage{
+	private Item i;
+	private double xp_apporte;
+	
+	/*Constructeur*/
+	public Pnj(String nom,double vie,Item i, double xp_apporte){
+	  super(0,nom,vie);
+	  this.i = i;
+	  this.xp_apporte = xp_apporte;
+	}
 
+	/*Getter et setter*/
+	public Item getI() {
+		return i;
+	}
+
+	public void setI(Item i) {
+		this.i = i;
+	}
+
+	public double getXp_apporte() {
+		return xp_apporte;
+	}
+
+	public void setXp_apporte(double xp_apporte) {
+		this.xp_apporte = xp_apporte;
+	}
+
+	/*ToString*/
+	@Override
+	public String toString() {
+		return "Pnj [i=" + i + ", xp_apporte=" + xp_apporte + "]";
+	}
+	
 }
