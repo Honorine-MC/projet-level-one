23e48ba7932ce8015f300c5890749371a6c1838b
honorine <honorine.mariechristy@gmail.com>
2021-05-17 10:53:13 +0200

    implémentation de la classe Potion

--- src/Potion.java
+++ src/Potion.java
@@ -1,4 +1,23 @@
 
-public class Potion {
+public class Potion extends Item {
+	private double vie_apporte;
+	
+	public Potion(int position, String nom,double vie_apporte){
+		super(position,nom);
+		this.vie_apporte = vie_apporte;		
+	}
 
+	public double getVie_apporte() {
+		return vie_apporte;
+	}
+
+	public void setVie_apporte(double vie_apporte) {
+		this.vie_apporte = vie_apporte;
+	}
+
+	@Override
+	public String toString() {
+		return "Potion [vie_apporte=" + vie_apporte + "]";
+	}
+	
 }
