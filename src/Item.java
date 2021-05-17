b28a2cb1a76a2333eaae7627d2649a41895474e1
honorine <honorine.mariechristy@gmail.com>
2021-05-17 10:49:49 +0200

    Implémentation de la classe Item

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,23 @@
 
-public class Item {
+public class Item extends Element{
+	private String nom;
+	
+	public Item(int position, String nom){
+		super(position);
+		this.nom = nom;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [nom=" + nom + "]";
+	}
+	
 }
