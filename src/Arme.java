77576c0ea20fffd7e8313fb2566a0bc6481f6a85
honorine <honorine.mariechristy@gmail.com>
2021-05-17 10:55:32 +0200

    implémentation de la classe arme

--- src/Arme.java
+++ src/Arme.java
@@ -1,4 +1,23 @@
 
-public class Arme {
+public class Arme extends Item{
+	private double degat_apporte;
+	
+	public Arme(int position, String nom, double degat_apporte){
+		super(position,nom);
+		this.degat_apporte=degat_apporte;
+	}
 
+	public double getDegat_apporte() {
+		return degat_apporte;
+	}
+
+	public void setDegat_apporte(double degat_apporte) {
+		this.degat_apporte = degat_apporte;
+	}
+
+	@Override
+	public String toString() {
+		return "Arme [degat_apporte=" + degat_apporte + "]";
+	}
+	
 }
