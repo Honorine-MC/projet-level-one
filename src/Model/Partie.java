452f5349ccdc89ebf280a9891d9dc203259ac096
MohamedKhemar <mohamedkhemar@gmail.com>
2021-05-19 14:03:44 +0200

    HALALAA

--- src/Model/Partie.java
+++ src/Model/Partie.java
@@ -5,7 +5,7 @@ public class Partie {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map map = new Map(5,5);
+		Map map = new Map(6,6);
 		map.init();
 		map.add();
 		map.affiche();
