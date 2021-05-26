36784d9e329dbd922e424fd65cc5742b6c3809de
honorine <honorine.mariechristy@gmail.com>
2021-05-26 10:53:24 +0200

    Ajout de l'inventaire dans l'interface

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,14 +1,23 @@
 package Vue;
 
+import java.awt.Label;
+import java.util.ArrayList;
+
 import Model.Case;
 import Model.Element;
+import Model.Item;
+import Model.Joueur;
 import Model.Map;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
@@ -20,15 +29,21 @@ public class Vue extends Application{
 	public void start(Stage primaryStage) throws Exception{
 		try{
 			VBox root = new VBox();
-			Scene scene = new Scene(root,800,800);
+			Scene scene = new Scene(root,1000,800);
 			scene.setFill(Color.CADETBLUE);
 			
 			GridPane plateau = new GridPane();
+			plateau.setPadding(new Insets(40, 40, 40, 40));
+			plateau.setMinSize(300, 300);
+//			plateau.setVgap(5);
+//			plateau.setHgap(5);
+			 
 			root.getChildren().add(plateau);
 			
 			map.init();
 			
 			Case[][] plateauTab = map.plateau;
+			
 			
 			for(int i=0; i<8; i++){
 				for(int j=0; j<8;j++){
@@ -46,6 +61,43 @@ public class Vue extends Application{
 					plateau.add(caseGridPane, i, j);
 				}
 			}
+					
+			
+			
+	
+			/* Affichage de l'inventaire*/
+
+			Joueur joueur = map.joueur;
+			GridPane inventaire = new GridPane();
+			root.getChildren().add(inventaire);
+			inventaire.setPadding(new Insets(40, 40, 40, 800));
+			
+			if(joueur.getInventaire().size() > 0){
+				for(int i=0 ; i< joueur.getInventaire().size(); i++ ){
+					Rectangle carre = new Rectangle(0,0,30,30);
+					carre.setFill(Color.WHITE);
+					carre.setStroke(Color.BLACK);
+					Image image = new Image(joueur.getInventaire().get(i).getImage());
+					carre.setFill(new ImagePattern(image));				
+					GridPane caseGridPane = new GridPane();
+					caseGridPane.getChildren().add(carre);
+					inventaire.add(caseGridPane,i,10);
+	
+				}
+			}
+			else{
+				for(int i=0; i<5;i++){
+					Rectangle carre = new Rectangle(0,0,30,30);
+					carre.setFill(Color.WHITE);
+					carre.setStroke(Color.BLACK);
+					GridPane caseGridPane = new GridPane();
+					caseGridPane.getChildren().add(carre);
+					inventaire.add(caseGridPane, i, 10);
+					
+				}
+			}
+			
+			
 			primaryStage.setTitle("LEVEL ONE");
 			primaryStage.setScene(scene);
 			primaryStage.show();
